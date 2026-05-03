82e718c881d1f746ce999757dcd6745be1855608
douglassutil <douglassutil@gmail.com>
2026-05-03 02:19:59 -0300

    chore: add basic App component scaffold with TODO instructions

---
+++ apps/frontend/src/App.tsx
@@ -0,0 +1,22 @@
+function App() {
+  return (
+    <div>
+      <h1>Starter React Beginner</h1>
+
+      {/*
+        TODO (Student Task):
+
+        Replace this content with your first UI.
+
+        Suggestions:
+
+        - Create a layout
+        - Create a navigation menu
+        - Display system status
+      */}
+
+    </div>
+  );
+}
+
+export default App;
