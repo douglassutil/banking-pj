77587ea72fb66c0581c5d6e80fac8cbdaf2d92f5
douglassutil <douglassutil@gmail.com>
2026-05-03 02:17:24 -0300

    chore: add empty AppModule scaffold for student

---
+++ apps/backend/src/app.module.ts
@@ -0,0 +1,15 @@
+import { Module } from '@nestjs/common';
+
+@Module({
+  imports: [],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}
+
+/**
+ * TODO (Student Task):
+ *
+ * Register your controllers here.
+ *
+ */
