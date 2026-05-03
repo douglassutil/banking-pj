58d2fde7fce3ed8bf8a1915c2908da962353cb69
douglassutil <douglassutil@gmail.com>
2026-05-03 02:19:43 -0300

    chore: add frontend main.tsx scaffold with TODO instructions

---
+++ apps/frontend/src/main.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+/**
+ * TODO (Student Task):
+ *
+ * 1. Create your first page component
+ * 2. Display a simple message on the screen
+ * 3. Example: "System running"
+ */
